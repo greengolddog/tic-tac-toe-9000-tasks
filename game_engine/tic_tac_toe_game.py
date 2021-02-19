259ec6f3472a137c17d3baa90e727e8b73780096
greengolddog <loseff.pit@ya.ru>
2021-02-19 12:41:15 +0300

    fix bugs

--- game_engine/tic_tac_toe_game.py
+++ game_engine/tic_tac_toe_game.py
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 from copy import deepcopy
-from .tic_tac_toe_common_lib import *
+from .tic_tac_toe_common_lib import AbstractTicTacToeGame, TicTacToeGameInfo, TicTacToeTurn
+from typing import Callable
 
 
 class TicTacToeGame(AbstractTicTacToeGame):
@@ -15,7 +15,7 @@ class TicTacToeGame(AbstractTicTacToeGame):
         self.field = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
         self.sequence_of_turns = list()
 
-    def who_is_win(self) -> str:
+    def winner(self) -> str:
         c = 0
         for i in range(3):
             bx = 0
@@ -99,9 +99,9 @@ class TicTacToeGame(AbstractTicTacToeGame):
     def is_turn_correct(self, turn: TicTacToeTurn) -> bool:
         if self.winner_id == '':
             if (turn.x_coordinate >= 0)\
-             and (turn.y_coordinate >= 0)\
-              and (turn.x_coordinate < 3)\
-               and (turn.y_coordinate < 3):
+            and (turn.y_coordinate >= 0)\
+            and (turn.x_coordinate < 3)\
+            and (turn.y_coordinate < 3):
                 if self.now_player_id == turn.player_id:
                     if self.field[turn.x_coordinate][turn.y_coordinate] == ' ':
                         return True
@@ -119,54 +119,9 @@ class TicTacToeGame(AbstractTicTacToeGame):
                 self.field[turn.x_coordinate][turn.y_coordinate] = 'X'
             if turn.player_id == self.second_player_id:
                 self.field[turn.x_coordinate][turn.y_coordinate] = 'O'
-            self.winner_id = self.who_is_win()
+            self.winner_id = self.winner()
             if self.now_player_id == self.first_player_id:
                 self.now_player_id = self.second_player_id
             else:
                 self.now_player_id = self.first_player_id
         return self.get_game_info()
-=======
-from copy import deepcopy
-from typing import Callable, List, Optional
-from .tic_tac_toe_common_lib import TicTacToeTurn, TicTacToeGameInfo, AbstractTicTacToeGame
-
-
-class TicTacToeGame(AbstractTicTacToeGame):
-    """Наследуемся от абстрактного класса и реализуем ручками все методы"""
-
-    def __init__(self, game_id: str, first_player_id: str, second_player_id: str,
-                 strategy: Optional[Callable[[TicTacToeGameInfo], TicTacToeTurn]] = None) -> None:
-        self._game_id = game_id
-        self._first_player_id = first_player_id
-        self._second_player_id = second_player_id
-        self._winner_id = ""
-        self._strategy = strategy
-        self._turns: List[TicTacToeTurn] = []
-
-    def is_turn_correct(self, turn: TicTacToeTurn) -> bool:
-        raise NotImplementedError
-
-    def do_turn(self, turn: TicTacToeTurn) -> TicTacToeGameInfo:
-        raise NotImplementedError
-
-    def get_game_info(self) -> TicTacToeGameInfo:
-        result = TicTacToeGameInfo(
-            game_id=self._game_id,
-            field=[
-                [" ", " ", " "],
-                [" ", " ", " "],
-                [" ", " ", " "]
-            ],
-            sequence_of_turns=deepcopy(self._turns),
-            first_player_id=self._first_player_id,
-            second_player_id=self._second_player_id,
-            winner_id=self._winner_id
-        )
-        for turn in self._turns:
-            if turn.player_id == self._first_player_id:
-                symb = "X"
-            else:
-                symb = "O"
-            result.field[turn.x_coordinate][turn.y_coordinate] = symb
-        return result
->>>>>>> tasks

2012a58f9138bcedd3b16200362c4703c93930d6
greengolddog <loseff.pit@ya.ru>
2021-02-19 18:13:25 +0300

    add tests and app

--- game_app/app.py
+++ game_app/app.py
@@ -2,6 +2,7 @@ from typing import Dict
 
 from game_engine import TicTacToeGame, TicTacToeGameInfo, TicTacToeTurn
 
+from uuid import uuid4
 
 class TicTacToeApp:
     def __init__(self):
@@ -10,10 +11,13 @@ class TicTacToeApp:
         self._games: Dict[str, TicTacToeGame] = {}
 
     def start_game(self, first_player_id: str, second_player_id: str) -> TicTacToeGameInfo:
-        raise NotImplementedError
+        game_id = uuid4()
+        self._games[game_id] = TicTacToeGame(\
+        game_id, first_player_id, second_player_id)
+        return self._games[game_id].get_game_info()
 
-    def get_game_by_id(self, game_id: str, user_id: str) -> TicTacToeGameInfo:
-        raise NotImplementedError
+    def get_game_by_id(self, game_id: str) -> TicTacToeGameInfo:
+        return self._games[game_id].get_game_info()
 
     def do_turn(self, turn: TicTacToeTurn, game_id: str) -> TicTacToeGameInfo:
-        raise NotImplementedError
+        return self._games[game_id].do_turn(turn)
